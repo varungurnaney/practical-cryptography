8ec901edf7672bc5d50ad4728bdef6775b812760
varun gurnaney <varungurnaney@gmail.com>
2018-09-13 19:38:34 -0400

    check file err

--- helloworld.go
+++ helloworld.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-
 )
 
-func main() {
-	if (len(os.Args[1:]) != 2) 
-	{
-	fmt.Println(" Usage: vigenere-encrypt <key> <plaintext filename\n")
-	} 
-	else 
-	{
-		key := os.Args[1]
-		/* the assignment says that the key should be all uppercase chars less than 32 chars */
-		if (len(key) > 32) {
-			fmt.Println(" Key should be uppercase chars less than 32 chars \n")
-		  }
-	}	
+func checkReadFile(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
+func main() {
+	if len(os.Args[1:]) != 2 {
+		fmt.Println(" Usage: vigenere-encrypt <key> <plaintext file> \n")
+
+	} else {
+
+		key := os.Args[1]
+		if len(key) > 32 {
+			fmt.Println(" Key should be uppercase and of 32 characters \n")
+		}
+
+		plaintextFile := os.Args[2]
+		fmt.Println("Plaintext file being used: " + plaintextFile)
+
+		/* open and read plaintext file */
+		data, err := ioutil.ReadFile(plaintextFile)
+		checkReadFile(err)
+
+	}
 }
