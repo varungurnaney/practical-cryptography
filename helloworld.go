413faafa8476b28b86a69379ec5f467820cdbcd4
varun gurnaney <varungurnaney@gmail.com>
2018-09-13 19:18:43 -0400

    args count

--- helloworld.go
+++ helloworld.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	"fmt"
+	"os"
+
+)
+
+func main() {
+	if (len(os.Args[1:]) != 2) 
+	{
+	fmt.Println(" Usage: vigenere-encrypt <key> <plaintext filename\n")
+	} 
+	else 
+	{
+		key := os.Args[1]
+		/* the assignment says that the key should be all uppercase chars less than 32 chars */
+		if (len(key) > 32) {
+			fmt.Println(" Key should be uppercase chars less than 32 chars \n")
+		  }
+	}	
+
+}
